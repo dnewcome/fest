ceb506e0e269e8132f310942284ddd402e4b6026
dnewcome <djn125@yahoo.com>
2011-07-24 21:47:31 -0700

    release build script and updates to project file to support building multiple configurations

---
+++ build.bat
@@ -0,0 +1,15 @@
+:: assuming 4.0 compiler is available
+set pointversion=1.0.0
+
+:: set msbuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+set msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
+
+set projfile=fest.csproj
+
+:: note that we MUST specify /t:rebuild otherwise just changing the target framework isn't enough to 
+:: force uptodate logic to rebuild the project. We'll end up with 2 identical builds of whatever is first
+%msbuild% %projfile% /p:Configuration=Debug /p:TargetFrameworkVersion=v4.0 /p:PointVersion=%pointversion% /t:rebuild
+%msbuild% %projfile% /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:PointVersion=%pointversion% /t:rebuild
+ 
+%msbuild% %projfile% /p:Configuration=Debug /p:TargetFrameworkVersion=v2.0 /p:PointVersion=%pointversion% /t:rebuild
+%msbuild% %projfile% /p:Configuration=Release /p:TargetFrameworkVersion=v2.0 /p:PointVersion=%pointversion% /t:rebuild
